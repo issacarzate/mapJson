e85717ee9ffc216446fee66f7127fe6c17277d83
Issac Arzate Soto <issac@MacBook-Pro-de-Issac.local>
2018-03-09 13:26:33 -0600

    Se puede anadir un marcado al dar click sobre el mapa y si se quiere guardar en el servidor se da click sobre el marcador posicionado. Al dar click sobre el boton Challenge, se devuelven del servidor todos los marcadores guardados con aterioridad

--- app/src/main/java/com/example/issac/maps/MapsActivity.java
+++ app/src/main/java/com/example/issac/maps/MapsActivity.java
@@ -11,7 +11,12 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,6 +24,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.issac.maps.pojo.City;
+import com.example.issac.maps.pojo.MarkerInfo;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
@@ -28,17 +34,24 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MapsActivity extends FragmentActivity implements OnMarkerClickListener, OnMapReadyCallback, GoogleApiClient.
         ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener
@@ -52,6 +65,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private final int REQUEST_LOCATION_CODE = 99;
     private RequestQueue mQueue;
 
+            @Override
+            public boolean onMarkerClick( Marker marker) {
+
+                // Retrieve the data from the marker.
+                Integer clickCount = (Integer) marker.getTag();
+
+                // Check if a click count was set, then display the click count.
+                if (clickCount != null) {
+                    clickCount = clickCount + 1;
+                    marker.setTag(clickCount);
+                    Toast.makeText(this,
+                            marker.getTitle() +
+                                    " has been clicked " + clickCount + " times.",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+                // Return false to indicate that we have not consumed the event and that we wish
+                // for the default behavior to occur (which is for the camera to move such that the
+                // marker is centered and for the marker's info window to open, if it has one).
+                return false;
+            }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,10 +97,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
 
+
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+
+            @Override
+            public void onMapReady(final GoogleMap googleMap) {
+
+                mMap = googleMap;
+
+
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+
+                    @Override
+                    public void onMapClick(final LatLng latLng) {
+
+                        // Creating a marker
+                        MarkerOptions markerOptions = new MarkerOptions();
+
+
+
+                        // Setting the position for the marker
+                        markerOptions.position(latLng);
+
+                        // Setting the title for the marker.
+                        // This will be displayed on taping the marker
+                        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+
+                        // Clears the previously touched position
+                        mMap.clear();
+
+                        // Animating to the touched position
+                        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                        // Placing a marker on the touched position
+                        mMap.addMarker(markerOptions);
+
+                        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                            @Override
+                            public boolean onMarkerClick(final Marker marker) {
+                                    Toast.makeText(MapsActivity.this, marker.getTitle() + " Se guardará... ", Toast.LENGTH_SHORT).show();
+                                    String lati =Double.toString(latLng.latitude);
+                                    String longi = Double.toString(latLng.longitude);
+                                    postCities(lati, longi);
+
+                                // Return false to indicate that we have not consumed the event and that we wish
+                                // for the default behavior to occur (which is for the camera to move such that the
+                                // marker is centered and for the marker's info window to open, if it has one).
+                                return false;
+                                    }
+                            });
+                    }
+                });
+            }
+        });
         mQueue = VolleySingleton.getInstance(this).getRequestQueue();
     }
+
+
 
     //Para verificar si se tiene o no el permiso por parte del usuario
 
@@ -85,11 +176,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
                 break;
             case R.id.challenge:
-                getCities("http://api.geonames.org/citiesJSON?formatted=true&north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=perciplyr&style=full");
+                getCities("http://lenmiapi.azurewebsites.net/api/service/points");
                 //http://api.geonames.org/citiesJSON?formatted=true&north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=perciplyr&style=full
 
                 break;
         }
+
     }
 
     public boolean checkLocationPermission()
@@ -114,20 +206,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private void getCities (String url){
 
+
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
                     //JSONObject data = response.getJSONObject("data");
-                    JSONArray jsonArray = response.getJSONArray("geonames");
+                    JSONArray jsonArray = response.getJSONArray("Points");
                     for (int i=0 ; i<jsonArray.length() ; i++){
                         JSONObject jsonObject = jsonArray.getJSONObject(i);
 
                         City city = new City();
                         //marvelDude.id = jsonObject.getLong("id") + "";
-                        city.lat = jsonObject.getString("lat");
-                        city.lon = jsonObject.getString("lng");
-                        city.name = jsonObject.getString("name");
+                        city.lat = jsonObject.getString("Latitud");
+                        city.lon = jsonObject.getString("Longitud");
+                        city.name = jsonObject.getString("Nombre");
 
                         LatLng latLng = new LatLng(Double.parseDouble(city.lat), Double.parseDouble(city.lon));
                         MarkerOptions markerOptions = new MarkerOptions();
@@ -148,9 +241,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
         });
+        mQueue = VolleySingleton.getInstance(this).getRequestQueue();
+
 
         mQueue.add(request);
     }
+
+            private void postCities (final String lat, final String lng){
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            URL url = new URL("http://lenmiapi.azurewebsites.net/api/service/place");
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod("POST");
+                            conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                            conn.setRequestProperty("Accept","application/json");
+                            conn.setDoOutput(true);
+                            conn.setDoInput(true);
+
+                            JSONObject jsonParam = new JSONObject();
+                            jsonParam.put("Nombre", "Name");
+                            jsonParam.put("Latitud", lat);
+                            jsonParam.put("Longitud", lng);
+
+                            Log.i("JSON", jsonParam.toString());
+                            DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                            //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+                            os.writeBytes(jsonParam.toString());
+
+                            os.flush();
+                            os.close();
+
+                            Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                            Log.i("MSG" , conn.getResponseMessage());
+
+                            conn.disconnect();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                thread.start();
+            }
+
 
 
     /**
@@ -162,9 +297,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        this.mMap = googleMap;
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED){
@@ -172,6 +308,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .addApi(LocationServices.API).build();
             googleApiClient.connect();
             mMap.setMyLocationEnabled(true);
+            mMap.setOnMarkerClickListener(this);
+
         }
 /*
         // Add a marker in Sydney and move the camera
@@ -225,4 +363,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
             }
+
+
+
+
+
         }
---
+++ app/src/main/java/com/example/issac/maps/pojo/MarkerInfo.java
@@ -0,0 +1,23 @@
+package com.example.issac.maps.pojo;
+
+/**
+ * Created by issac on 08/03/18.
+ */
+
+public class MarkerInfo {
+    private String title;
+
+    public MarkerInfo() { }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public MarkerInfo(String title) {
+        this.title = title;
+    }
+}
